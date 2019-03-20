b8b0e909caa38c40aebc2610ee3e99e22159fa1c
buster731 <enahs731@gmail.com>
2019-03-20 09:51:51 -0400

    I think this code should work. next step printing things

--- src/hands/hand.java
+++ src/hands/hand.java
@@ -1,7 +1,50 @@
+package hands;
+
+import java.util.ArrayList;
+
 import Card.Card;
 import setup.profile;
 
 public class hand{
+	public int size;
+	public int handVal;
+	public int handPoints;
+	public ArrayList<Card> holding;
 	//make this the constructor, make dealer and player instances of it
 	//public hand()
+	public hand() {
+		this.holding = new ArrayList<Card>();
+		this.size = holding.size();
+		this.handPoints = handValue();
+	}
+	
+	public int handValue() {
+		handVal = 0;
+		int ace = 0;
+		for(Card item : holding) {
+			if(item.rank() == "ace") {
+				ace++;
+				handVal += 10;
+			}
+			handVal += item.pointValue();
+		}
+		if(handVal == 21) {
+			return handVal;
+		}
+		else if(handVal > 21) {
+			while(ace > 0 && handVal > 21) {
+				handVal -= 10;
+				ace--;
+			}
+			return handVal;
+		}
+		else {
+			return handVal;
+		}
+	}
+	
+	public int getHandPoints() {
+		return this.handPoints;
+	}
+	
 }
--- src/setup/play.java
+++ src/setup/play.java
@@ -3,12 +3,46 @@ import java.util.Scanner;
 
 import Card.Card;
 import Deck.Deck;
-import setup.actions;
 import setup.profile;
+import hands.*;
 
 
 
 public class play{
+	public static hand playerHand = new hand();
+	public static hand dealerHand = new hand();
+	
+	private static Deck decks;
+
+	public static void hit(hand right) {
+		Card c = decks.deal();
+		right.holding.add(c);
+	}
+	
+	public void checkHand(hand user, hand dealer) {
+		int points = user.handValue();
+		if(points == 21) {
+			//win
+		}
+		else if(points > 21) {
+			//bust
+		}
+		else if(dealer.handValue() > 21) {
+			//win
+		}
+		else if(points > dealer.handValue()) {
+			//win
+		}
+		else {
+			//push
+		}
+	}
+	
+	public static void start(hand person) {
+		hit(person);
+		hit(person);
+	}
+	
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		System.out.println("What is your name?");
@@ -17,23 +51,26 @@ public class play{
 		int cashout = -1;
 		
 		System.out.println("Welcome " + profile.getName() + "!");
-		
+		start(playerHand);
+		start(dealerHand);
 		while(cashout != 0) {
 			System.out.println("There is no active hand yet. What would you like to do?");
 			String action = in.nextLine();
 			
 			switch(action) {
 			case "play":
+				
 				int over = -1;
 				while(over != 0) {
 					System.out.println("What would you like to do?");
 					String hist = in.next();
 					switch(hist) {
 					case "hit":
-						//hit
+						hit(playerHand);
+						//print hand and stuff
 						break;
 					case "stand":
-						//dealer play
+						//check stuff
 						break;
 					default:
 						System.out.println("Not a valid action. Available actions are: /n/t- hit /n/t- stand /n");
