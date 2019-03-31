e696987171293a35b31ac7d7874f5c4b94372685
buster731 <43239906+buster731@users.noreply.github.com>
2019-03-31 19:40:50 -0400

    finished blackjack

--- src/setup/play.java
+++ src/setup/play.java
@@ -13,9 +13,7 @@ public class play{
 	
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		hand playerHand = new hand(new ArrayList<Card>());
-		hand dealerHand = new hand(new ArrayList<Card>());
-		table newTable = new table(playerHand, dealerHand);
+		
 		System.out.println("What is your name?");
 		profile.setName(in.nextLine());
 		profile.setChips(5000);
@@ -23,30 +21,75 @@ public class play{
 		
 		System.out.println("Welcome " + profile.getName() + "!");
 		
-		newTable.playerStart(playerHand);
-		newTable.dealerStart(dealerHand);
-		playerHand.holding = playerHand.sortHand();
-		dealerHand.holding = dealerHand.sortHand();
+		
 		while(cashout != 0) {
+			System.out.println();
 			System.out.println("There is no active hand yet. What would you like to do?");
 			String action = in.nextLine();
 			
 			switch(action) {
 			case "play":
-				
+				hand playerHand = new hand(new ArrayList<Card>());
+				hand dealerHand = new hand(new ArrayList<Card>());
+				table newTable = new table(playerHand, dealerHand);
+				long chipBet = -1;
+				System.out.println("How much would you like to bet?");
+				String bet = in.nextLine();
+				boolean isValid = false;
+				while(isValid != true) {
+					isValid = true;
+					for(int i = 0; i < bet.length(); i++) {
+						if(Character.isDigit(bet.charAt(i)) == false) {
+							isValid = false;
+						}
+					}
+					if(isValid == false) {
+						System.out.println("Invalid bet amount");
+						System.out.println("How much would you like to bet?");
+						bet = in.nextLine();
+					}
+				}
+				chipBet = Long.parseLong(bet);
+				while(chipBet < 0 || chipBet > profile.getChips() || isValid == false) {
+					isValid = true;
+					for(int i = 0; i < bet.length(); i++) {
+						if(Character.isDigit(bet.charAt(i)) == false) {
+							isValid = false;
+						}
+					}
+					System.out.println("Invalid bet amount");
+					System.out.println("How much would you like to bet?");
+					bet = in.nextLine();
+					chipBet = Long.parseLong(bet);
+				}
+				long chipsRem = profile.getChips() - chipBet;
+				profile.setChips(chipsRem);
+				newTable.playerStart(playerHand);
+				newTable.dealerStart(dealerHand);
+				playerHand.holding = playerHand.sortHand();
+				dealerHand.holding = dealerHand.sortHand();
 				int over = -1;
 				boolean bust = false;
 				while(over != 0 && bust != true) {
+					if(playerHand.handValue() == 21 && dealerHand.handValue() != 21) {
+						System.out.println("BLACKJACK!");
+						newTable.win(chipBet);
+						over = 0;
+						break;
+					}
 					System.out.println("What would you like to do?");
 					String hist = in.next();
 					switch(hist) {
 					case "hit":
 						newTable.hit(playerHand);
-						bust = newTable.checkHand(playerHand);
+						bust = newTable.checkHand(playerHand, bust);
+						if(bust == true) {
+							newTable.lose();
+						}
 						//print hand and stuff
 						break;
 					case "stand":
-						newTable.stand(playerHand, dealerHand);
+						newTable.stand(playerHand, dealerHand, chipBet);
 						over = 0;
 						//check stuff
 						break;
